a2576e9738e67a7bfcf50068175bd3ff2aa92357
Erik Bryn <erik.bryn@gmail.com>
2013-04-26 11:39:13 -0400

    Add exists helper

--- test/javascripts/integration/bloggr_test.js
+++ test/javascripts/integration/bloggr_test.js
@@ -11,7 +11,7 @@ test("/", function() {
   expect(1);
 
   visit("/").then(function() {
-    ok($('.navbar').length, "The navbar is rendered");
+    ok(exists(".navbar"), "The navbar was rendered");
   });
 });
 
@@ -19,8 +19,8 @@ test("/posts", function() {
   expect(2);
 
   visit("/posts").then(function() {
-    ok(find('th:contains(Recent Posts)').length, "Recent posts list is rendered");
-    ok(find('*:contains(Please select a post)').length);
+    ok(exists('th:contains(Recent Posts)'), "Recent posts list is rendered");
+    ok(exists('*:contains(Please select a post)'));
   });
 });
 
@@ -31,7 +31,7 @@ test("/posts/:post_id", function() {
     return click("td a:first");
   }).then(function() {
     equal(find(".span9 h1").text(), "zomg", "The post title was rendered");
-    ok(find(".span9 h2:contains(by ebryn)").length, "The post author was rendered");
+    ok(exists(".span9 h2:contains(by ebryn)"), "The post author was rendered");
     equal(find(".span9 .intro").text().trim(), "zomg", "The post intro was rendered");
     equal(find(".span9 .below-the-fold").text().trim(), "ZOMG ZOMG ZOMG ZOMG", "The post intro was rendered");
   })
