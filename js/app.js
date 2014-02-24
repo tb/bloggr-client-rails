95690e316f9961ee61c9ef3f2c8b5911ed8bb9bb
jeffreybiles <bilesjeffrey@gmail.com>
2014-02-24 02:24:32 -0600

    less rails-centric blog posts

--- js/app.js
+++ js/app.js
@@ -2,19 +2,18 @@ App = Ember.Application.create({});
 
 var posts = [{
   id: '1',
-  title: "Rails is Omakase",
-  author: { name: "d2h" },
-  date: new Date('12-27-2012'),
-  excerpt: "There are lots of à la carte software environments in this world. Places where in order to eat, you must first carefully look over the menu of options to order exactly what you want.",
-  body: "I want this for my ORM, I want that for my template language, and let's finish it off with this routing library. Of course, you're going to have to know what you want, and you'll rarely have your horizon expanded if you always order the same thing, but there it is. It's a very popular way of consuming software.\n\nRails is not that. Rails is omakase."
+  title: "Taco Tuesday",
+  author: { name: "President Business" },
+  date: new Date('2-8-2014'),
+  excerpt: "",
+  body: "Hi, I'm President Business, president of the Octan corporation and the world. Let's take extra care to follow the instructions or you'll be *put to sleep*, and don't forget Taco Tuesday's coming next week."
 }, {
   id: '2',
-  title: "The Parley Letter",
-  author: { name: "d2h" },
-  date: new Date('12-24-2012'),
-  excerpt: "My [appearance on the Ruby Rogues podcast](http://rubyrogues.com/056-rr-david-heinemeier-hansson/) recently came up for discussion again on the private Parley mailing list.",
-  body: "A long list of topics were raised and I took a time to ramble at large about all of them at once. Apologies for not taking the time to be more succinct, but at least each topic has a header so you can skip stuff you don't care about.\n\n### Maintainability\n\nIt's simply not true to say that I don't care about maintainability. I still work on the oldest Rails app in the world."  
-}];
+  title: "Top 3 reasons why Everything is Awesome",
+  author: { name: "President Business" },
+  date: new Date('2-7-2014'),
+  excerpt: "",
+  body: "1. You're part of a team\n\n2. We're living our dream\n\n3. Gonna win forever, party forever"}];
 
 App.Router.map(function() {
   this.resource('about');
